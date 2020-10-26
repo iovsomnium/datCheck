a2e84bf2e9707b1c9639a9281d6da30fa5b8584b
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2020-10-26 17:35:50 +0900

    add static files

--- datCheck/urls.py
+++ datCheck/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from mainPage import views as index_views
 from django.urls import path,re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('mainPage/', index_views.helloWorld)
 ]
